19bdc95a5d588b478f7e9055b8b818fb2fae5904
jzelldincht <jzelldincht@gmali.com>
2022-04-06 23:58:41 +0800

    增加一个static，对比下

---
+++ frontend-projects/blog-static/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="main">
+      Index Page
+
+      <nuxt-link :to="{name:'articles-id', params: {id: 1}, query: {a: 1, b: 2}}">文章详情</nuxt-link>
+
+      <el-button type="primary" round>主要按钮</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
